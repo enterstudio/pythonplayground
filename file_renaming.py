bd29eb95ea8706d0fba075c85309c357b5d54d27
Krs Joseph <krspjoseph@gmail.com>
2016-11-17 00:41:06 -0400

    add before and after filenames

--- file_renaming.py
+++ file_renaming.py
@@ -1,5 +1,6 @@
 import os
 
+
 def rename_files():
     dir = "/Users/infotechtestmac/sites/pythonplayground/prank_after"
     saved_path = os.getcwd()
@@ -8,6 +9,7 @@ def rename_files():
 
     for file_name in file_list:
         os.rename(file_name, file_name.translate(None, "0123456789"))
+        print "File named " + file_name + " has been renamed to " + file_name.translate(None, "0123456789")
 
     os.chdir(saved_path)
 
