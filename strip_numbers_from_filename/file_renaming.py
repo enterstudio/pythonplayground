59303d2bc7f098df490b61412ebcc845acf69c73
Krs Joseph <krspjoseph@gmail.com>
2016-11-17 10:06:57 -0400

    Make code more modular

--- strip_numbers_from_filename/file_renaming.py
+++ strip_numbers_from_filename/file_renaming.py
@@ -2,15 +2,18 @@ import os
 
 
 def rename_files():
-    dir = "/Users/infotechtestmac/Downloads/alphabet"
-    saved_path = os.getcwd()
-    file_list = os.listdir(dir)
-    os.chdir(dir)
+    root_dir = os.getcwd()
+    files_dir = root_dir + "/prank"
+    file_list = os.listdir(files_dir)
+    os.chdir(files_dir)
 
     for file_name in file_list:
         os.rename(file_name, file_name.translate(None, "0123456789"))
-        print "File named " + file_name + " has been renamed to " + file_name.translate(None, "0123456789")
+        print "The file named " \
+              + file_name \
+              + " has been renamed to " \
+              + file_name.translate(None, "0123456789")
 
-    os.chdir(saved_path)
+    os.chdir(root_dir)
 
 rename_files()
--- take_a_break/break_time.py
+++ take_a_break/break_time.py
@@ -3,10 +3,15 @@ import time
 
 total_breaks = 3
 break_count = 0
+play_count = 0
 
-print "The current time is" + time.ctime()
 while break_count < total_breaks:
     time.sleep(2*60*60)
     webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
     break_count = break_count+1
+    play_count = play_count + 1
+    print "The current time is " \
+          + time.ctime() \
+          + " and the play count is " \
+          + str(play_count)
 
